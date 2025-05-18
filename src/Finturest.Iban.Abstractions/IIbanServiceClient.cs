e6df44a666554ec02e9d968fbb94ff89bc0a63bb
finturest-support <support@finturest.com>
2025-05-18 13:49:52 +0200

    Added IbanException

--- src/Finturest.Iban.Abstractions/IIbanServiceClient.cs
+++ src/Finturest.Iban.Abstractions/IIbanServiceClient.cs
@@ -16,5 +16,6 @@ public interface IIbanServiceClient
     /// <exception cref="ArgumentNullException">The request model was null.</exception>
     /// <exception cref="InvalidOperationException">The request failed due to deserialization issue.</exception>
     /// <exception cref="HttpRequestException">The request failed due to an underlying issue such as network connectivity, DNS failure, server certificate validation or timeout.</exception>
+    /// <exception cref="IbanException">Thrown when the input values violate business rules specific to the IBAN generation logic (e.g., unsupported country, invalid structure, or failed checksum).</exception>
     Task<GenerateIbanResponseApiModel> GenerateIbanAsync(GenerateIbanRequestApiModel request, CancellationToken cancellationToken = default);
 }
--- tests/Finturest.Iban.IntegrationTests/IbanServiceClientGenerateIbanIntegrationTests.cs
+++ tests/Finturest.Iban.IntegrationTests/IbanServiceClientGenerateIbanIntegrationTests.cs
@@ -24,4 +24,25 @@ public partial class IbanServiceClientIntegrationTests
 
         result.Iban.ShouldBe("FR7630006000011234567890189");
     }
+
+    [Fact]
+    public async Task GenerateIbanRequestApiModel_RequestIsValid_ReturnCorrectResult()
+    {
+        // Arrange
+        var request = new GenerateIbanRequestApiModel
+        {
+            CountryCode = "FR",
+            BankIdentifier = "30006",
+            BranchIdentifier = "00001",
+            BankAccountNumber = "1234567890189"
+        };
+
+        // Act
+        var result = await _sut.GenerateIbanAsync(request);
+
+        // Assert
+        result.ShouldNotBeNull();
+
+        result.Iban.ShouldBe("FR7630006000011234567890189");
+    }
 }
