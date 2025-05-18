04fa4dd999c52585f90ef709bbb7a79c9580e7be
finturest-support <support@finturest.com>
2025-05-18 13:40:41 +0200

    Added test ti generate iban

---
+++ tests/Finturest.Iban.IntegrationTests/IbanServiceClientGenerateIbanIntegrationTests.cs
@@ -0,0 +1,27 @@
+﻿using Finturest.Iban.Abstractions.Models.Requests;
+
+namespace Finturest.Iban.IntegrationTests;
+
+public partial class IbanServiceClientIntegrationTests
+{
+    [Fact]
+    public async Task GenerateIbanRequestApiModel_RequestIsValid_ReturnCorrectResult()
+    {
+        // Arrange
+        var request = new GenerateIbanRequestApiModel
+        {
+            CountryCode = "FR",
+            BankIdentifier = "30006",
+            BranchIdentifier = "00001",
+            BankAccountNumber = "1234567890189"
+        };
+
+        // Act
+        var result = await _sut.GenerateIbanAsync(request);
+
+        // Assert
+        result.ShouldNotBeNull();
+
+        result.Iban.ShouldBe("FR7630006000011234567890189");
+    }
+}
---
+++ tests/Finturest.Iban.IntegrationTests/IbanServiceClientIntegrationTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Net;
+using System.Net.Http;
+
+using Finturest.Iban.Abstractions;
+using Finturest.Iban.Abstractions.Models.Requests;
+using Finturest.Iban.DependencyInjection;
+
+using Microsoft.Extensions.Configuration;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Finturest.Iban.IntegrationTests;
+
+/// <summary>
+/// Integration tests for <see cref="IIbanServiceClient"/>
+/// </summary>
+public partial class IbanServiceClientIntegrationTests
+{
+    private readonly IIbanServiceClient _sut;
+
+    public IbanServiceClientIntegrationTests()
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddEnvironmentVariables()
+#if DEBUG
+            .AddUserSecrets<IbanServiceClientIntegrationTests>()
+#endif
+            .Build();
+
+        var apiKey = configuration["Iban:ApiKey"] ?? throw new InvalidOperationException("Finturest IBAN API key must be set in environment or user secrets.");
+
+        _sut = BuildClient(apiKey);
+    }
+
+    [Fact]
+    public async Task SendRequestAsync_ApiKeyIsNotValid_EnsureUnauthorizedStatusCode()
+    {
+        // Arrange
+        var request = new GenerateIbanRequestApiModel
+        {
+            CountryCode = "PL",
+            BankAccountNumber = "123456789"
+        };
+
+        // Act
+        Func<Task> action = async () => await BuildClient(apiKey: "invalid-api-key").GenerateIbanAsync(request).ConfigureAwait(false);
+
+        // Assert
+#if NET5_0_OR_GREATER
+        var assertion = await action.ShouldThrowAsync<HttpRequestException>();
+
+        assertion.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+#else
+        await action.ShouldThrowAsync<HttpRequestException>();
+#endif
+    }
+
+    private static IIbanServiceClient BuildClient(string apiKey)
+    {
+        var services = new ServiceCollection();
+
+        services.AddFinturestIban(options =>
+        {
+            options.ApiKey = apiKey;
+        });
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        return serviceProvider.GetRequiredService<IIbanServiceClient>();
+    }
+}
